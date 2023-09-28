18dc703f7c212f17dcebdc2c7331cad3691aad79
Saurav Kumar <maxsaurav237@gmail.com>
2023-09-28 18:13:55 +0530

    final commit

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { BottomNavigation } from "./BottomNavigation";
+export { HomeHeader } from "./HomeHeader";
+export { ProductCard } from "./ProductCard";
+export { BackNavigation } from "./BackNavigation";
+export { CartItem } from "./CartItem";
+export { PriceDetails } from "./PriceDetails";
